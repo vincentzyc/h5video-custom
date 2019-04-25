a41b7fb4243156e37ac44bed52d3604e3fac35e9
Vincentyun <vincentzyc@qq.com>
2019-04-25 14:27:07 +0800

    init

--- js/videoplayer.es6.js
+++
@@ -1,9 +0,0 @@
-/* ES6 Syntax*/
-(function(window, document) {
-    class videoPlayer {
-        constructor(video) {
-            this.id = video.id ? video.id : '';
-            this.videoContainer = document.getElementById(this.id);
-        }
-    }
-}(this, document))
